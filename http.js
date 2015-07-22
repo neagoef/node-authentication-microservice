64ee8ee364411845d617984957137dd489e4a61b
AJ ONeal <awesome@coolaj86.com>
2015-07-21 18:58:34 -0600

    partial work

---
+++ http.js
@@ -0,0 +1,76 @@
+'use strict';
+
+// This service is intended to be bound on 127.0.0.1,
+// DO NOT expose it to the internet
+module.exports = function () {
+  var router = require('express').Router();
+  var bodyParser = require('body-parser');
+  var db;
+
+  // if multiple cores, use http
+  // if single core, require directly
+  router.use('/', function (req, res, next) {
+    if (!db) {
+      res.send({ error: { code: 'E_NO_INIT', message: "database must be initialized" } });
+      return;
+    }
+
+    next();
+  });
+
+  router.use('/', bodyParser.json());
+
+  router.get('/init', function (req, res) {
+    var authdb = require('./index');
+    var conf = req.body;
+
+    if (db) {
+      res.send({ error: { code: 'E_INIT_COMPLETE', message: 'the initialization has already happened' } });
+      return;
+    }
+
+    // TODO get ROOT on create
+    // TOOD check key, bits, storage
+    authdb.create({
+      key: conf.key
+    , bits: conf.bits
+    , storage: conf.storage // './authn.sqlcipher'
+    }).then(function (_db) {
+      db = _db;
+      // TODO send root user details?
+      res.send({ success: true });
+    }, function (err) {
+      // it's safe to send the raw message because
+      // this is an internal microservice
+      res.send({ error: { message: err.message } });
+    });
+  });
+
+  router.get('/values/:id', function (req, res) {
+    var id = req.params.id;
+
+    db.get(id).then(function (data) {
+      res.send(data);
+    }, function (err) {
+      res.send({ error: { message: err.message } });
+    });
+  });
+
+  /*
+  router.post('/values/new', function (req, res) {
+  });
+  */
+
+  router.post('/values/:id', function (req, res) {
+    var id = req.params.id;
+    var data = req.body;
+
+    db.upsert(id, data).then(function () {
+      res.send({ success: true });
+    }, function (err) {
+      res.send({ error: { message: err.message } });
+    });
+  });
+
+  return router;
+};
---
+++ index.js
@@ -0,0 +1,144 @@
+'use strict';
+
+var PromiseA = require('bluebird');
+var sqlite3 = require('./sqlite-server');
+var path = require('path');
+
+sqlite3.create({
+    key: '1892d335081d8d346e556c9c3c8ff2c3'
+  , bits: 128
+  , storage: path.join('/tmp/authn.sqlcipher')
+  , verbose: false
+}).then(function (db) {
+  var DB = {};
+  var tablename = 'authn';
+
+  db = PromiseA.promisifyAll(db);
+
+  db.on('trace', function (str) {
+    console.log('SQL:', str);
+  });
+
+  db.on('profile', function (sql, ms) {
+    console.log('Profile:', ms);
+  });
+
+  DB.get = function (id) {
+    return db.getAsync("SELECT * FROM authn WHERE id = ?", [id]);
+  };
+
+  DB.upsert = function (id, data) {
+    return DB.set(id, data).then(function (result) {
+      var success = result.changes >= 1;
+
+      if (success) {
+        return result;
+      }
+
+      return DB.create(id, data);
+    });
+  };
+
+  DB.create = function (id, data) {
+    var json = JSON.stringify(data);
+
+    return new PromiseA(function (resolve, reject) {
+      db.run("INSERT INTO authn (id, json) VALUES (?, ?)", [id, json], function (err) {
+        if (err) {
+          reject(err);
+          return;
+        }
+
+        // NOTE changes is 1 even if the value of the updated record stays the same
+        // (PostgreSQL would return 0 in that case)
+        // thus if changes is 0 then it failed, otherwise it succeeded
+        console.log(this); // sql, lastID, changes
+        console.log(this.sql);
+        console.log('insert lastID', this.lastID); // sqlite's internal rowId
+        console.log('insert changes', this.changes);
+
+        resolve(this);
+      });
+    });
+  };
+
+  DB.set = function (id, data) {
+    var json = JSON.stringify(data);
+
+    return new PromiseA(function (resolve, reject) {
+      db.run("UPDATE authn SET json = ? WHERE id = ?", [json, id], function (err) {
+        if (err) {
+          reject(err);
+          return;
+        }
+
+        // it isn't possible to tell if the update succeeded or failed
+        // only if the update resulted in a change or not
+        console.log(this); // sql, lastID, changes
+        console.log(this.sql);
+        console.log('update lastID', this.lastID); // always 0 (except on INSERT)
+        console.log('update changes', this.changes);
+
+        resolve(this);
+      });
+    });
+  };
+
+  return new PromiseA(function (resolve, reject) {
+    db.serialize(function() {
+      PromiseA.all([
+        db.runAsync("CREATE TABLE IF NOT EXISTS '" + sqlite3.sanitize(tablename)
+          + "' (id TEXT, secret TEXT, json TEXT, PRIMARY KEY(id))")
+      ]).then(function () { resolve(DB); }, reject);
+    });
+  });
+}).then(function (DB) {
+  var data = { secret: 'super secret', verifiedAt: 1437207288791 };
+  //return DB.set('aj@the.dj', data)
+  //return DB.set('coolaj86@gmail.com', data)
+  // return DB.upsert('awesome@coolaj86.com', data)
+  return DB.upsert('awesome@coolaj86.com', data).then(function () {
+    console.log('added user');
+  });
+
+  /*
+  return DB.create('coolaj86@gmail.com', data).then(function () {
+    console.log('added user');
+  });
+  */
+
+  // need to 'DELETE FROM authn;' first
+  return DB.get('coolaj86@gmail.com').then(function (user) {
+    if (user) {
+      console.log('user', user);
+      return;
+    }
+
+    //var data = { secret: 'super secret', verifiedAt: Date.now() };
+    var data = { secret: 'super secret', verifiedAt: 1437207288790 };
+    return DB.create('coolaj86@gmail.com', data).then(function () {
+      console.log('added user');
+    });
+
+  });
+}).then(function () {}, function (err) {
+  // code SQLITE_CONSTRAINT
+  // errno 19
+
+  console.error('blah');
+  //console.error(Object.keys(err)); // errno, code
+  console.error(err);
+
+});
+
+/*
+if (require.main === module) {
+  // crypto.randomBytes(16).toString('hex');
+  create({
+    key: '1892d335081d8d346e556c9c3c8ff2c3'
+  , bits: 128
+  , storage: './authn.sqlcipher'
+  }).then(function (DB) {
+  });
+}
+*/
