95e4a0d3c5501ee70bc81799b781d284c30b8f80
AJ ONeal <awesome@coolaj86.com>
2015-09-01 02:53:58 -0600

    tdd-ing

---
+++ tests/login.js
@@ -0,0 +1,313 @@
+'use strict';
+
+/*global Promise*/
+var PromiseA = Promise;
+try {
+  PromiseA = require('bluebird').Promise;
+} catch (e) {
+  // ignore
+}
+
+function dbsetup() {
+  var config = require('../config.test.js');
+  var sqlite3 = require('sqlite3-cluster');
+  var wrap = require('dbwrap');
+
+  var dir = [
+    { tablename: 'codes'
+    , idname: 'uuid'
+    , indices: ['createdAt']
+    }
+  , { tablename: 'logins' // coolaj86, coolaj86@gmail.com, +1-317-426-6525
+    , idname: 'hashId'
+    //, relations: [{ tablename: 'secrets', id: 'hashid', fk: 'loginId' }]
+    , indices: ['createdAt', 'type', 'node']
+    //, immutable: false
+    }
+  , { tablename: 'verifications'
+    , idname: 'hashId' // hash(date + node)
+    //, relations: [{ tablename: 'secrets', id: 'hashid', fk: 'loginId' }]
+    , indices: ['createdAt', 'nodeId']
+    //, immutable: true
+    }
+  , { tablename: 'secrets'
+    , idname: 'hashId' // hash(node + secret)
+    , indices: ['createdAt']
+    //, immutable: true
+    }
+  , { tablename: 'recoveryNodes' // just for 1st-party logins
+    , idname: 'hashId' //
+      // TODO how transmit that something should be deleted / disabled?
+    , indices: ['createdAt', 'updatedAt', 'loginHash', 'recoveryNode', 'deleted']
+    }
+  ];
+
+  var promise = sqlite3.create({
+      standalone: true
+    , bits: 128
+    , filename: config.filename
+    , verbose: false
+  });
+
+  return promise.then(function (db) {
+    return db.init({ bits: 128, key: config.key });
+  }).then(function (db) {
+    return wrap.wrap(db, dir);
+  });
+
+  /*
+  if (require.main === module) {
+    create({
+      key: '1892d335081d8d346e556c9c3c8ff2c3'
+    , bits: 128
+    , filename: '/tmp/authn.sqlcipher'
+    }).then(function (DB) {
+    });
+  }
+  */
+}
+
+function init(Logins) {
+  var tests;
+  var count = 0;
+  var kdfMeta = {
+    salt: 'hex'
+  , algo: 'pbkdf2'
+  , iter: 678
+  };
+
+  function setup() {
+    return PromiseA.resolve();
+  }
+
+  function teardown() {
+    return PromiseA.resolve();
+  }
+
+  // Test that success is successful
+  tests = [
+    function () {
+      // test setup / teardown
+      return PromiseA.resolve();
+    }
+  , function failUnknownType() {
+      // fail because 'instagram' type is not implemented
+      return Logins.create({
+        node: '#coolaj86'
+      , type: 'instagram'
+      , secret: 'TODO_PROOF_OF_SECRET'
+      }).then(function () {
+        throw new Error("didn't fail to create unsupported type");
+      }).error(function (err) {
+        if (/instagram/.test(err.message)) {
+          return;
+        }
+
+        throw err;
+      });
+    }
+  , function passRecoverable() {
+      // success because it's inherently recoverable
+      return Logins.create({
+        node: 'coolaj86@gmail.com'
+      , type: 'email' // could be something like slack as well
+      , secret: 'TODO_PROOF_OF_SECRET'
+      }).then(function () {
+        console.log('double yay');
+      });
+    }
+  , function failUnrecoverable() {
+      console.log('failUnrec');
+      // fail because there's no recoverable
+      return Logins.create({
+        node: 'coolaj86'
+      , type: 'username'
+      , secret: 'TODO_PROOF_OF_SECRET'
+      , recoveryNodes: [{ node: 'farmwood' }]
+      }).then(function () {
+        throw new Error("didn't fail to create unrecoverable account");
+      }).error(function (err) {
+        if (/recoverable/.test(err.message)) {
+          return;
+        }
+
+        throw err;
+      });
+    }
+  , function failUnsecured() {
+      // fail because there's no secret
+      return Logins.create({
+        node: 'coolaj86'
+      , type: 'username'
+      , recoveryNodes: [{ node: 'john.doe@email.com' }]
+      }).then(function () {
+        throw new Error("didn't fail to create unsecured username account");
+      });
+    }
+  , function passUnsecuredEmail() {
+      // pass because we can login via login code to email
+      return Logins.create({
+        node: 'coolaj86@gmail.com'
+      , type: 'email'
+      }).then(function () {
+        throw new Error("didn't fail to create unsecured username account");
+      });
+    }
+  , function notVerified() {
+      // return false
+      return Logins.isVerified({
+        type: 'email'
+      , node: 'coolaj86@gmail.com'
+      }).then(function (recoverable) {
+        if (recoverable) {
+          throw new Error('unverified email should not be recoverable');
+        }
+      });
+    }
+  , function verify() {
+      // nothing to test here, actually, just setup for the next function
+      Logins.claim({
+        type: 'email'
+      , node: 'coolaj86@gmail.com'
+      }).then(function (claim) {
+        // Note: failing with a bad claim is tested in authcodes tests
+        return Logins.validateClaim({
+          type: 'email'
+        , node: 'coolaj86@gmail.com'
+          // soft expirey
+        , expiresIn: 180 * 24 * 60 * 60 * 1000
+        , claim: claim
+        });
+      });
+    }
+  , function passVerified() {
+      // returns true
+      return Logins.isVerified({
+        type: 'email'
+      , node: 'coolaj86@gmail.com'
+      });
+    }
+  , function createUsername() {
+      // succeed with notice that account is verified recoverable
+      return Logins.create({
+        node: 'coolaj86'
+      , type: 'username'
+      , recoveryNodes: ['coolaj86@gmail.com']
+      , secret: 'TODO_PROOF_OF_SECRET'
+        // kdf meta
+      , salt: kdfMeta.salt
+      , algo: kdfMeta.algo
+      , iter: kdfMeta.iter
+      });
+    }
+  , function failInvalidCreds() {
+      return Logins.login({
+        node: 'coolaj86'
+      , type: 'username'
+      , secret: 'NOT_MY_SECRET'
+      }).error(function (err) {
+        console.error('fail invalid creds');
+        console.error(err);
+        throw err;
+      });
+    }
+  , function passGetMeta() {
+      return Logins.login({
+        node: 'coolaj86'
+      , type: 'username'
+      }).then(function (meta) {
+        if (!meta) {
+          throw new Error('missing meta');
+        }
+
+        if (meta.algo !== kdfMeta.algo) {
+          throw new Error('bad algo');
+        }
+        if (meta.iter !== kdfMeta.iter) {
+          throw new Error('bad iter');
+        }
+        if (meta.salt !== kdfMeta.salt) {
+          throw new Error('bad salt');
+        }
+      });
+    }
+  , function passValidCreds() {
+      // Succeed
+      Logins.login({
+        id: 'coolaj86'
+      , type: 'username'
+      , secret: 'TODO_PROOF_OF_SECRET'
+      });
+    }
+  , function failChangePassword() {
+      Logins.changePassword({
+        id: 'coolaj86'
+      , type: 'username'
+      , secret: 'NOT_MY_SECRET'
+      , oldSecret: 'TODO_PROOF_OF_SECRET'
+      });
+    }
+  , function passChangePassword() {
+      Logins.changePassword({
+        id: 'coolaj86'
+      , type: 'username'
+      , secret: 'TODO_PROOF_OF_SECRET'
+      , oldSecret: 'TODO_PROOF_OF_SECRET'
+      });
+    }
+  ];
+
+  var testsLen = tests.length;
+
+  function phase1() {
+    return new PromiseA(function (resolve) {
+      var curFn;
+
+      function callDoStuff() {
+        curFn = tests.shift();
+        return doStuff(curFn, testsLen - tests.length).error(function (err) {
+          err.fn = curFn;
+          return teardown().then(function () {
+            throw err;
+          });
+        });
+      }
+
+      function doStuff(fn, i) {
+        console.log('i1', i);
+        return setup().then(fn).then(teardown).then(function () {
+          console.log('i2', i, count);
+          count += 1;
+
+          return callDoStuff();
+        });
+      }
+
+      callDoStuff().then(function () {
+        console.log('weirdness');
+        resolve();
+      }).catch(function (err) {
+        console.error('[ERROR] failure');
+        console.error(err);
+        console.error(err.fn.toString());
+        resolve();
+      });
+    });
+  }
+
+  phase1().then(function () {
+    console.info('%d of %d tests complete', count, testsLen);
+    process.exit();
+  });
+}
+
+module.exports.create = function () {
+
+  dbsetup().then(function (DB) {
+    return init(
+      require('../lib/login-impl').create({}, require('authcodes').create(DB.Codes), DB)
+    );
+  });
+};
+
+module.exports.create();
